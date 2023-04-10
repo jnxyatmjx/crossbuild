ea0bb89b0274311159ac75a276a194092746a551
jnxyatmjx <jnxyatmjx@qq.com>
2023-04-10 11:19:15 +0800

    add local shadowsocks-libev.mk build file

---
+++ shadowsocks-libev.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# shadowsocks-libev
+#
+################################################################################
+
+#SHADOWSOCKS_LIBEV_VERSION = 2023.04.07-101717
+SHADOWSOCKS_LIBEV_SITE = https://github.com/jnxyatmjx/shadowsocks-libev/archive/refs/tags/2023.04.07-101717.tar.gz
+#SHADOWSOCKS_LIBEV_LICENSE = GPL-3.0+, BSD-2-Clause (libbloom), BSD-3-Clause (libcork, libipset)
+#SHADOWSOCKS_LIBEV_LICENSE_FILES = COPYING libbloom/LICENSE libcork/COPYING
+SHADOWSOCKS_LIBEV_CPE_ID_VENDOR = shadowsocks
+SHADOWSOCKS_LIBEV_DEPENDENCIES = host-pkgconf c-ares libev libsodium mbedtls pcre
+SHADOWSOCKS_LIBEV_INSTALL_STAGING = YES
+# We're patching configure.ac
+SHADOWSOCKS_LIBEV_AUTORECONF = YES
+SHADOWSOCKS_LIBEV_CONF_OPTS = \
+        --with-pcre=$(STAGING_DIR)/usr \
+        --disable-ssp
+
+ifeq ($(BR2_PACKAGE_SHADOWSOCKS_LIBEV_CONNMARKTOS),y)
+SHADOWSOCKS_LIBEV_DEPENDENCIES += libnetfilter_conntrack
+SHADOWSOCKS_LIBEV_CONF_OPTS += --enable-connmarktos
+else
+SHADOWSOCKS_LIBEV_CONF_OPTS += --disable-connmarktos
+endif
+
+$(eval $(autotools-package))
